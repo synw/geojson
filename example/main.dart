075f639e35e46599c9e82ccc1d6e4abb1fa27c45
Win <synwe@yahoo.com>
2019-07-28 02:51:45 +0000

    Update example

--- example/main.dart
+++ example/main.dart
@@ -11,17 +11,41 @@ void main() async {
 
 void polygons(File file) async {
   final file = File("lakes_of_europe.geojson");
+  final features = await featuresFromGeoJsonFile(file, nameProperty: "label");
+  for (final feature in features.collection) {
+    final geom = feature.geometry as MultiPolygon;
+    for (final polygon in geom.polygons) {
+      print("Polygon ${polygon.name}");
+      for (final geoSerie in polygon.geoSeries) {
+        print("${geoSerie.name}: " + "${geoSerie.geoPoints.length} geopoints");
+      }
+    }
+  }
+}
+
+void lines(File file) async {
+  final file = File("railroads_of_north_america.geojson");
+  final features =
+      await featuresFromGeoJsonFile(file, nameProperty: "continent");
+  for (final feature in features.collection) {
+    print("${feature.geometry.geoSerie.name}: " +
+        "${feature.geometry.geoSerie.geoPoints.length} geopoints");
+  }
+}
+/*
+void _polygons(File file) async {
+  final file = File("lakes_of_europe.geojson");
   final geoSeries = await geoSerieFromGeoJsonFile(file, nameProperty: "label");
   for (final geoSerie in geoSeries) {
     print("${geoSerie.name}: ${geoSerie.geoPoints.length} geopoints");
   }
 }
 
-void lines(File file) async {
+void _lines(File file) async {
   final file = File("railroads_of_north_america.geojson");
   final geoSeries =
       await geoSerieFromGeoJsonFile(file, nameProperty: "continent");
   for (final geoSerie in geoSeries) {
     print("${geoSerie.name}: ${geoSerie.geoPoints.length} geopoints");
   }
-}
+}*/
