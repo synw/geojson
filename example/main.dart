0636c2bfc03c68db173c56ded3e33d8da0f17d64
Win <synwe@yahoo.com>
2019-07-31 20:33:44 +0000

    Update example

--- example/main.dart
+++ example/main.dart
@@ -4,13 +4,24 @@ import 'package:geojson/geojson.dart';
 // data is from http://www.naturalearthdata.com
 
 void main() async {
-  multipolygons();
-  lines();
+  await smallData();
+  await multipolygons();
+  await lines();
 }
 
-void multipolygons() async {
+Future<void> smallData() async {
+  final file = File("small_data.geojson");
+  final features = await featuresFromGeoJsonFile(file, verbose: true);
+  for (final feature in features.collection) {
+    print("Point: ${feature.geometry.geoPoint.name}");
+  }
+  return;
+}
+
+Future<void> multipolygons() async {
   final file = File("lakes_of_europe.geojson");
-  final features = await featuresFromGeoJsonFile(file, nameProperty: "label");
+  final features =
+      await featuresFromGeoJsonFile(file, nameProperty: "label", verbose: true);
   for (final feature in features.collection) {
     final geom = feature.geometry as MultiPolygon;
     for (final polygon in geom.polygons) {
@@ -22,9 +33,9 @@ void multipolygons() async {
   }
 }
 
-void lines() async {
+Future<void> lines() async {
   final file = File("railroads_of_north_america.geojson");
-  final features = await featuresFromGeoJsonFile(file);
+  final features = await featuresFromGeoJsonFile(file, verbose: true);
   for (final feature in features.collection) {
     print("${feature.geometry.geoSerie.name}: " +
         "${feature.geometry.geoSerie.geoPoints.length} geopoints");
