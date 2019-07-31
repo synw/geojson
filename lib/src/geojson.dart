426ebf5112931327d2c86afc6db1348d32b90170
Win <synwe@yahoo.com>
2019-07-31 21:39:50 +0000

    Fixes in isolate runner

--- lib/src/geojson.dart
+++ lib/src/geojson.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 import 'package:iso/iso.dart';
+import 'package:meta/meta.dart';
 import 'models.dart';
 import 'deserializers.dart';
 import 'exceptions.dart';
@@ -10,19 +11,48 @@ import 'exceptions.dart';
 Future<FeatureCollection> featuresFromGeoJson(String data,
     {String nameProperty, bool verbose = false}) async {
   FeatureCollection featureCollection;
-  final iso = Iso(_processFeatures, onDataOut: (dynamic data) {
-    final _result = data as FeatureCollection;
-    featureCollection = _result;
-  });
-  await iso.run();
-  iso.dispose();
+  final finished = Completer<Null>();
+  Iso iso;
+  iso = Iso(_processFeatures,
+      onDataOut: (dynamic data) {
+        final _result = data as FeatureCollection;
+        featureCollection = _result;
+        iso.dispose();
+        finished.complete();
+      },
+      onError: (dynamic e) => throw (e));
+  final dataToProcess =
+      _DataToProcess(data: data, nameProperty: nameProperty, verbose: verbose);
+  await iso.run(<dynamic>[dataToProcess]);
+  await finished.future;
   return featureCollection;
 }
 
-void _processFeatures(IsoRunner iso) async {
+/// Get a feature collection from a geojson file
+Future<FeatureCollection> featuresFromGeoJsonFile(File file,
+    {String nameProperty, bool verbose = false}) async {
+  FeatureCollection features;
+  if (!file.existsSync()) {
+    throw ("File ${file.path} does not exist");
+  }
+  String data;
+  try {
+    data = await file.readAsString();
+  } catch (e) {
+    throw ("Can not read file $e");
+  }
+  features = await featuresFromGeoJson(data, verbose: verbose);
+  return features;
+}
+
+void _processFeatures(IsoRunner iso) {
   final List<dynamic> args = iso.args;
-  final data = args[0] as String;
-  final featuresCollection = _featuresFromGeoJson(data);
+  final dataToProcess = args[0] as _DataToProcess;
+  final String data = dataToProcess.data;
+  final String nameProperty = dataToProcess.nameProperty;
+  final bool verbose = dataToProcess.verbose;
+  final featuresCollection =
+      _featuresFromGeoJson(data, nameProperty: nameProperty, verbose: verbose);
   iso.send(featuresCollection);
 }
 
@@ -105,90 +135,13 @@ FeatureCollection _featuresFromGeoJson(String data,
   return features;
 }
 
-/// Get a feature collection from a geojson file
-Future<FeatureCollection> featuresFromGeoJsonFile(File file,
-    {String nameProperty, bool verbose = false}) async {
-  FeatureCollection features;
-  if (!file.existsSync()) {
-    throw ("File ${file.path} does not exist");
-  }
-  String data;
-  try {
-    data = await file.readAsString();
-  } catch (e) {
-    throw ("Can not read file $e");
-  }
-  features = await _featuresFromGeoJson(data, verbose: verbose);
-  return features;
-}
+class _DataToProcess {
+  _DataToProcess(
+      {@required this.data,
+      @required this.nameProperty,
+      @required this.verbose});
 
-/*
-/// Get a [GeoSerie] list from a geojson string
-Future<List<GeoSerie>> geoSerieFromGeoJson(String data,
-    {String nameProperty,
-    GeoSerieType type = GeoSerieType.group,
-    bool verbose = false}) async {
-  final result = <GeoSerie>[];
-  final Map<String, dynamic> decoded =
-      json.decode(data) as Map<String, dynamic>;
-  var i = 1;
-  final features = decoded["features"] as List<dynamic>;
-  for (final dfeature in features) {
-    final feature = dfeature as Map<String, dynamic>;
-    final properties = feature["properties"] as Map<String, dynamic>;
-    final geometry = feature["geometry"] as Map<String, dynamic>;
-    List<dynamic> coordsList;
-    final geomType = geometry["type"].toString();
-    final geoPoints = <GeoPoint>[];
-    String geomTypeLabel;
-    if (geomType == "MultiPolygon") {
-      geomTypeLabel = "Polygon";
-      final coordsL1 = geometry["coordinates"] as List<dynamic>;
-      final coordsL2 = coordsL1[0] as List<dynamic>;
-      coordsList = coordsL2[0] as List<dynamic>;
-      type = GeoSerieType.polygon;
-    } else if (geomType == "LineString") {
-      geomTypeLabel = "Line";
-      coordsList = geometry["coordinates"] as List<dynamic>;
-      type = GeoSerieType.line;
-    }
-    for (final coord in coordsList) {
-      final geoPoint = GeoPoint(
-          latitude: double.parse(coord[0].toString()),
-          longitude: double.parse(coord[1].toString()));
-      geoPoints.add(geoPoint);
-    }
-    String name;
-    if (nameProperty != null) {
-      name = properties[nameProperty].toString();
-    } else {
-      name = "$geomTypeLabel $i";
-    }
-    final geoSerie = GeoSerie(name: name, type: type, geoPoints: geoPoints);
-    result.add(geoSerie);
-    if (verbose) {
-      print("${geoSerie.name}: ${geoSerie.geoPoints.length} geopoints");
-    }
-    ++i;
-  }
-  return result;
+  final String data;
+  final String nameProperty;
+  final bool verbose;
 }
-
-/// Get a list of [GeoSerie] from a geojson file
-Future<List<GeoSerie>> geoSerieFromGeoJsonFile(File file,
-    {String nameProperty,
-    GeoSerieType type = GeoSerieType.group,
-    bool verbose = false}) async {
-  if (!file.existsSync()) {
-    throw ("File ${file.path} does not exist");
-  }
-  String data;
-  try {
-    data = await file.readAsString();
-  } catch (e) {
-    throw ("Can not read file $e");
-  }
-  return geoSerieFromGeoJson(data,
-      nameProperty: nameProperty, type: type, verbose: verbose);
-}
-*/
